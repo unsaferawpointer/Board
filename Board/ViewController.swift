065fc110c26a830236cc2e17457c180ab72f3571
Anton Cherkasov <unsaferawpointer@gmail.com>
2023-09-03 11:31:37 +0300

    Initial Commit

---
+++ Board/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Board
+//
+//  Created by Anton Cherkasov on 03.09.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
